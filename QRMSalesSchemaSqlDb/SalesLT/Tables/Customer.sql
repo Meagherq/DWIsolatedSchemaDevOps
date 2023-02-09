520abb775a1fa926b04e24b8eb02b11b64333a85
quinnmeagher <Quinn.Meagher@microsoft.com>
2023-02-09 11:39:59 -0500

    initial commit

---
+++ QRMSalesSchemaSqlDb/SalesLT/Tables/Customer.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [SalesLT].[Customer] (
+    [CustomerID]   INT               IDENTITY (1, 1) NOT NULL,
+    [NameStyle]    NVARCHAR (30) CONSTRAINT [DF_Customer_NameStyle] DEFAULT ((0)) NOT NULL,
+    [Title]        NVARCHAR (8)      NULL,
+    [FirstName]    NVARCHAR (30)     NULL,
+    [MiddleName]   NVARCHAR (30)     NULL,
+    [LastName]     NVARCHAR (30)      NULL,
+    [Suffix]       NVARCHAR (10)     NULL,
+    [CompanyName]  NVARCHAR (128)    NULL,
+    [SalesPerson]  NVARCHAR (256)    NULL,
+    [EmailAddress] NVARCHAR (50)     NULL,
+    [Phone]        NVARCHAR (30)     NULL,
+    [PasswordHash] VARCHAR (128)     NULL,
+    [PasswordSalt] VARCHAR (10)      NULL,
+    [rowguid]      UNIQUEIDENTIFIER  CONSTRAINT [DF_Customer_rowguid] DEFAULT (newid()) NOT NULL,
+    [ModifiedDate] DATETIME          CONSTRAINT [DF_Customer_ModifiedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Customer_CustomerID] PRIMARY KEY CLUSTERED ([CustomerID] ASC),
+    CONSTRAINT [AK_Customer_rowguid] UNIQUE NONCLUSTERED ([rowguid] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Customer_EmailAddress]
+    ON [SalesLT].[Customer]([EmailAddress] ASC);
+
